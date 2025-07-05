bac3db99bb57b20051336d6a9d14b40fd4e36621
MDilshodbek <mdbek55@gmail.com>
2025-07-05 18:04:48 +0900

    feat: mui customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import "@/styles/globals.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import light from "../scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("App Component - Pages Router");
+  const [theme, setTheme] = useState(createTheme(light));
 
-  return <Component {...pageProps} />;
+  // Soket.io, Redux, MUI ....
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />{" "}
+    </ThemeProvider>
+  );
 }
