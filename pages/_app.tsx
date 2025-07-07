30889d240a8ab25444acdd98adfa1cdef749eca1
MDilshodbek <mdbek55@gmail.com>
2025-07-07 12:50:29 +0900

    feat: publish main layout components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import light from "../scss/MaterialTheme";
 import { useState } from "react";
 import "../scss/app.scss";
+import "../scss/pc/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState(createTheme(light));
